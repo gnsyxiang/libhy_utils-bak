6db95d007b64a25b942746847fcf5b9bb467a53b
zhenquan.qiu <qiuzhenquan@unisound.com>
2019-11-07 14:36:39 +0800

    feat: <bitmap> add bitmap files

---
+++ src/bitmap_wrapper.c
@@ -0,0 +1,25 @@
+/**
+ * 
+ * Release under GPL-3.0.
+ * 
+ * @file    bitmap_wrapper.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    06/11 2019 20:23
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        06/11 2019      create the file
+ * 
+ *     last modified: 06/11 2019 20:23
+ */
+#include <stdio.h>
+
+#define LIBUTILS_INC_BITMAP_WRAPPER_GB
+#include "bitmap_wrapper.h"
+#undef LIBUTILS_INC_BITMAP_WRAPPER_GB
+
