5f84216bcd694c33c5e5106ef85a4aa86eb99381
zhenquan.qiu <gnsyxiang@163.com>
2020-03-21 14:43:39 +0800

    feat: add demo code

---
+++ example/utils-demo_test.c
@@ -0,0 +1,28 @@
+/**
+ * 
+ * Release under GPL-3.0.
+ * 
+ * @file    utils-demo_test.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    21/03 2020 14:14
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        21/03 2020      create the file
+ * 
+ *     last modified: 21/03 2020 14:14
+ */
+#include "hal_config.h"
+#include "utils-demo.h"
+
+hal_int32_t main(hal_int32_t argc, const hal_char *argv[])
+{
+    UtilsDemoFunc();
+
+    return 0;
+}
---
+++ src/utils-demo.c
@@ -0,0 +1,27 @@
+/**
+ * 
+ * Release under GPL-3.0.
+ * 
+ * @file    utils-demo.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    21/03 2020 14:13
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        21/03 2020      create the file
+ * 
+ *     last modified: 21/03 2020 14:13
+ */
+#include "hal_config.h"
+#include "utils-demo.h"
+
+void UtilsDemoFunc(void)
+{
+    Hal_LogT("hell demo test func \n");
+}
+
