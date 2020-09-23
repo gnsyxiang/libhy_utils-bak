1bc6790ea4d7845cfda958851a5486c74d1ae13e
zhenquan.qiu <qiuzhenquan@unisound.com>
2020-09-23 21:00:41 +0800

    doc: add doc for hy_log

--- include/hy_log.h
+++ include/hy_log.h
@@ -29,17 +29,40 @@ extern "C" {
 #define USE_DEBUG
 
 #ifdef USE_DEBUG
-enum {
+
+/**
+ * @brief 打印等级定义
+ */
+typedef enum {
     LOG_LEVEL_ERROR = 1,
     LOG_LEVEL_WARNING,
     LOG_LEVEL_DEBUG,
     LOG_LEVEL_INFO,
     LOG_LEVEL_MAX,
-};
+} LogLevel_t;
 
-// 下面函数和宏为内部用函数，最好不要在外面使用
-int32_t HyLogWrite(uint8_t level, const char *tags, const char *func, uint32_t line, char *fmt, ...);
+/**
+ * @brief 真实输出log函数
+ *
+ * @param level: 该条语句的打印等级
+ * @param tags: 打印类别
+ * @param func: 对应的函数
+ * @param line: 对应的行数
+ * @param fmt: 具体打印的内容
+ * @param ...
+ *
+ * @return 成功返回0
+ *
+ * note: 函数为内部用函数，最好不要在外面使用
+ */
+int32_t HyLogWrite(uint8_t level, const char *tags, const char *func,
+        uint32_t line, char *fmt, ...);
 
+/**
+ * @brief 输出log宏转义
+ *
+ * note: 宏为内部用，最好不要在外面使用
+ */
 #define LOG(no_debug, level, fmt, ...)                                  \
     do {                                                                \
         if (no_debug) {                                                 \
@@ -49,12 +72,29 @@ int32_t HyLogWrite(uint8_t level, const char *tags, const char *func, uint32_t l
     } while (0)
 #endif
 
-/*
- * API
+/**
+ * @brief 初始化log打印系统
+ *
+ * @param buf: 打印buf空间
+ * @param len: 打印buf空间的总长度
+ * @param level: 打印等级，详见LogLevel_t
+ *
+ * @return 成功返回0
+ *
+ * note: buf空间需要使用者维护的原因:
+ * 1, 使用者知道该空间何时创建，何时释放
+ * 2，对于单片机系统没有堆的概念，不能在内部使用malloc创建空间
  */
 int32_t HyLogInit(char *buf, uint32_t len, uint8_t level);
+
+/**
+ * @brief 销毁log系统
+ */
 void HyLogDestory(void);
 
+/**
+ * @brief 输出对应的log等级函数
+ */
 #ifdef USE_DEBUG
     #define	LOGE(fmt, ...)  LOG(ALONE_DEBUG, LOG_LEVEL_ERROR,    fmt, ##__VA_ARGS__)
     #define	LOGW(fmt, ...)  LOG(ALONE_DEBUG, LOG_LEVEL_WARNING,  fmt, ##__VA_ARGS__)
