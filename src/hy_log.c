38d593f57194701e59558df7a5b3644201522e7f
zhenquan.qiu <qiuzhenquan@unisound.com>
2020-10-09 13:58:05 +0800

    feat: add color for log_output

--- src/hy_log.c
+++ src/hy_log.c
@@ -25,6 +25,15 @@
 
 #ifdef USE_DEBUG
 
+#define ANSI_COLOR_RED     "\x1b[1;31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+/*printf("\033[字体高亮;字背景颜色;字体颜色m字符串\033[0m"); */
+
 typedef struct {
     char        *buf;
     uint32_t    len;
@@ -38,6 +47,7 @@ static log_context_t log_context;
 int32_t HyLogWrite(uint8_t level, const char *tags, const char *func,
         uint32_t line, char *fmt, ...)
 {
+#define SNPRINTF_FMT log_context.buf + ret, log_context.len - ret
     if (log_context.level < level) {
         return -1;
     }
@@ -45,13 +55,20 @@ int32_t HyLogWrite(uint8_t level, const char *tags, const char *func,
     uint32_t ret = 0;
     memset(log_context.buf, '\0', log_context.len);
 
-    ret += snprintf(log_context.buf + ret, log_context.len - ret,
-            "[%s][%s %d] ", tags, func, line); 
+    if (level <= LOG_LEVEL_WARNING) {
+        ret += snprintf(SNPRINTF_FMT, ANSI_COLOR_RED);
+    }
+
+    ret += snprintf(SNPRINTF_FMT, "[%s][%s %d] ", tags, func, line);
 
     va_list args;
     va_start(args, fmt);
-    vsnprintf(log_context.buf + ret, log_context.len - ret, fmt, args);
+    ret += vsnprintf(SNPRINTF_FMT, fmt, args);
     va_end(args);
+
+    if (level <= LOG_LEVEL_WARNING) {
+        ret += snprintf(SNPRINTF_FMT, ANSI_COLOR_RESET);
+    }
 
     printf("%s", log_context.buf);
 
