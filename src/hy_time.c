3e2ff0fce8bd25f2b6d5b8968136a44217f66f98
zhenquan.qiu <qiuzhenquan@unisound.com>
2020-11-27 08:28:02 +0800

    feat: add hy_time files

---
+++ src/hy_time.c
@@ -0,0 +1,41 @@
+/**
+ * 
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_time.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    27/11 2020 08:11
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        27/11 2020      create the file
+ * 
+ *     last modified: 27/11 2020 08:11
+ */
+#include <stdio.h>
+#include <sys/time.h>
+
+#include "hy_time.h"
+
+#define BASE_NUM	(1000)
+#define BASE_NUM_2	(1000 * 1000)
+#define BASE_NUM_3	(1000 * 1000 * 1000)
+
+#define take_integet_1000(num)		((num) / BASE_NUM)
+#define take_remainder_1000(num)	((num) % BASE_NUM)
+#define take_multiplier_1000(num)	((num) * BASE_NUM)
+
+time_t HyTimeGetTickMs(void)
+{
+    struct timeval tv;
+
+    gettimeofday(&tv, NULL);
+
+    return (take_multiplier_1000(tv.tv_sec) + take_integet_1000(tv.tv_usec));
+}
+
