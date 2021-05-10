b224c76d45a992c5d83cf0d7dafe855f6143c386
zhenquan.qiu <gnsyxiang@163.com>
2021-05-10 09:54:58 +0800

    fix: bubble sort struct

--- example/hy_sort_test.c
+++ example/hy_sort_test.c
@@ -28,15 +28,15 @@
 #define ALONE_DEBUG 1
 #define LOG_CATEGORY_TAG "main"
 
-static hy_int32_t _swap_cb(void *src, void *dst)
+static hy_int32_t _swap_int_cb(void *src, void *dst)
 {
-    int *a = src;
-    int *b = dst;
+    hy_int32_t  *a = src;
+    hy_int32_t  *b = dst;
 
     return (*a > *b);
 }
 
-static void _dump(hy_int32_t *a, hy_uint32_t len)
+static void _dump_int(hy_int32_t *a, hy_uint32_t len)
 {
     for (hy_uint32_t i = 0; i < len; ++i) {
         printf("%d ", *(a + i));
@@ -44,20 +44,67 @@ static void _dump(hy_int32_t *a, hy_uint32_t len)
     printf("\n");
 }
 
+static void _test_int(void)
+{
+    hy_int32_t a[] = {3, 4, 1, 5, 8, 0, 9};
+    hy_uint32_t len = HY_ARRAY_CNT(a);
+
+    _dump_int(a, len);
+    HySortBubble(a, len, sizeof(a[0]),  _swap_int_cb);
+    _dump_int(a, len);
+}
+
+/*! \struct _student
+ *  \brief Brief struct description
+ *
+ *  Detailed description
+ */
+typedef struct _student {
+    hy_uint32_t id;         /*!< Description */
+    char        name[32];
+    float       score[3];
+} student_t;
+
+static hy_int32_t _swap_stu_cb(void *src, void *dst)
+{
+    student_t *a = src;
+    student_t *b = dst;
+
+    return a->score[0] > b->score[0];
+    // return a->id > b->id;
+}
+
+static void _dum_student(student_t *stu, hy_uint32_t len)
+{
+    for (hy_uint32_t i = 0; i < len; ++i) {
+        printf("id: %d, name: %s, score: %f, %f, %f \n",
+                stu[i].id, stu[i].name, stu[i].score[0],stu[i].score[1], stu[i].score[2]);
+    }
+}
+
+static void _test_struct(void)
+{
+    student_t stu[] = {
+        {1001, "tom", {77, 22, 33}},
+        {1007, "jim", {44, 55, 99}},
+        {1003, "jac", {11, 88, 66}},
+    };
+    hy_uint32_t len = HY_ARRAY_CNT(stu);
+
+    _dum_student(stu, len);
+    HySortBubble(stu, len, sizeof(stu[0]),  _swap_stu_cb);
+    _dum_student(stu, len);
+}
+
 int main(int argc, char const* argv[])
 {
     HyLogCreate(HY_LOG_LEVEL_INFO, 512);
 
-    int a[] = {3, 4, 1, 5, 8, 0, 9};
-    int len = HY_ARRAY_CNT(a);
-
-    _dump(a, len);
-
-    HySortBubble(a, len, sizeof(a[0]),  _swap_cb);
-
-    _dump(a, len);
+    _test_int();
+    _test_struct();
 
     HyLogDestroy();
 
     return 0;
 }
+
