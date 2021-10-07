7cc2b0b057c7704bc1e5f72dadb568f333b66f2e
zhenquan.qiu <gnsyxiang@163.com>
2021-10-07 11:10:46 +0800

    feat: add hy_error file

---
+++ include/hy_error.h
@@ -0,0 +1,39 @@
+/**
+ *
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_error.h
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    07/05 2021 20:07
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        07/05 2021      create the file
+ * 
+ *     last modified: 07/05 2021 20:07
+ */
+#ifndef __LIBHY_UTILS_INCLUDE_HY_ERROR_H_
+#define __LIBHY_UTILS_INCLUDE_HY_ERROR_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+    HY_ERR_FAILD = -1,
+    HY_ERR_OK = 0,
+
+    HY_ERR_MAX,
+} HyErr_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
